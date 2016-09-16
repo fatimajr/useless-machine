d5eea5f19c3afa3c1be4407bd8247682e4f873ab
fatima <fatima.jacoboruiz@gmail.com>
2016-09-16 09:08:35 -0500

    actualización colores

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 window.addEventListener("load", function() {
-    var boton = document.getElementById("boton1");
+    var boton = document.getElementById("cri");
+    var contador = 1;
     boton.addEventListener("click", function() {
-        document.getElementById("inputcolor").value = "Blue";
-		document.getElementById("color1").classList.toggle("border-blue");
+    	if(contador == 1) {
+		document.getElementById("inputcolor").value = "Azul";
+		document.getElementById("azul").classList.add("border-blue");
+		document.getElementById("amarillo").classList.remove("border-yellow");
+		}
+		if(contador == 2) {
+		document.getElementById("inputcolor").value = "Rojo";
+		document.getElementById("rojo").classList.add("border-red");
+		document.getElementById("azul").classList.remove("border-blue");
+		}
+        if(contador == 3) {
+		document.getElementById("inputcolor").value = "Amarillo";
+		document.getElementById("amarillo").classList.add("border-yellow");
+		document.getElementById("rojo").classList.remove("border-red");
+		}
+		contador ++;
+	});
+});
+	window.addEventListener("load", function() {
+	var boton = document.getElementById("supercri");
+	boton.addEventListener("click", function() {
+		var color = document.getElementById("inputcolor").value;
+		if (color == "azul") {
+			document.getElementById("azul").classList.add("border-blue");
+			document.getElementById("rojo").classList.remove("border-red");
+			document.getElementById("amarillo").classList.remove("border-yellow");
+		} else if (color == "rojo") {
+			document.getElementById("rojo").classList.add("border-red");
+			document.getElementById("azul").classList.remove("border-blue");
+			document.getElementById("amarillo").classList.remove("border-yellow");
+		} else if (color == "amarillo") {
+			document.getElementById("amarillo").classList.add("border-yellow");
+			document.getElementById("rojo").classList.remove("border-red");
+			document.getElementById("azul").classList.remove("border-blue");
+		}
 	});
 });
